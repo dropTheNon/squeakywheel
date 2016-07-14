eca50b433e7b121bb61b05e07fc96a196a1662bd
Josh McBroom <mcbroomjc@gmail.com>
2016-07-14 10:03:21 -0700

    Main controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root 'main#index'
+
+  get 'signup', to: 'user#new'
+
+  post 'signup', to: 'user#create'
+
+  get 'profile', to: 'user#show'
+
+  get 'user/edit', to: 'user#edit'
+
+  # put 'user#update'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
