3f116a09271d92a4ee9954f7a69f083aa68b8235
Josh McBroom <mcbroomjc@gmail.com>
2016-07-15 17:49:56 -0700

    OmniAuth-Facebook added, but not tested. Need to add links to login via Facebook and test. Doing that tomorrow. It is date night.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713223343) do
+ActiveRecord::Schema.define(version: 20160716000645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,10 @@ ActiveRecord::Schema.define(version: 20160713223343) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "provider_id"
+    t.string   "provider_token"
+    t.string   "provider_name"
+    t.text     "picture"
   end
 
   create_table "users_complaints", force: :cascade do |t|
